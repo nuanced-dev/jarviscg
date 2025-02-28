db40ab2953835719962781471ff203024de19af2
Laila Winner <lailawinner@protonmail.ch>
2025-02-28 08:56:29 -0800

    Remove redundant test

--- tests/jarviscg/call_graph_generator_test.py
+++ tests/jarviscg/call_graph_generator_test.py
@@ -13,54 +13,13 @@ def change_directory():
     yield
     os.chdir("../")
 
-def test_call_graph_generator_processes_files_depth_first() -> None:
-    entrypoints = [
-            "./fixtures/plugins.py",
-            "./fixtures/__init__.py",
-            "./fixtures/lazyframe/frame.py",
-            "./fixtures/lazyframe/__init__.py",
-            "./fixtures/_utils/parse/expr.py",
-            "./fixtures/_utils/__init__.py",
-            "./fixtures/_utils/parse/__init__.py",
-    ]
-    expected = {
-        "fixtures.plugins": [],
-        "fixtures": [],
-        "fixtures.lazyframe.frame": ["fixtures.lazyframe.frame.LazyFrame"],
-        'fixtures.lazyframe.LazyFrame': ["fixtures.lazyframe.frame.LazyFrame"],
-        "fixtures.lazyframe.frame.LazyFrame": [],
-        "fixtures.lazyframe": [],
-        "fixtures._utils.parse.expr": [],
-        "fixtures._utils": [],
-        "fixtures._utils.parse": [],
-        "fixtures.lazyframe.frame.LazyFrame.group_by": ["fixtures._utils.parse.parse_into_list_of_expressions"],
-        "fixtures.plugins.register_plugin_function": ["fixtures._utils.parse.parse_into_list_of_expressions"],
-        "fixtures._utils.parse.parse_into_list_of_expressions": ["fixtures._utils.parse.expr.parse_into_list_of_expressions"],
-        "fixtures._utils.parse.expr.parse_into_list_of_expressions": ["fixtures._utils.parse.expr._parse_positional_inputs"],
-        "fixtures._utils.parse.expr._parse_positional_inputs": [],
-    }
-
-    cg = CallGraphGenerator(entrypoints, None)
-    cg.analyze()
-
-    formatter = formats.Simple(cg)
-    output = formatter.generate()
-
-    diff = DeepDiff(expected, output, ignore_order=True)
-    assert diff == {}
-
 def test_call_graph_generator_handles_exports() -> None:
-    # Explaining this test:
+    # Fixture setup:
     # - `klass.py` imports `parse_into_list_of_expressions` from `_utils.parse`
     # - `Klass#method` invokes `parse.expr.parse_into_list_of_expressions`
     # - `_utils/parse/__init__.py` exports `parse_into_list_of_expressions`
     # using `__all__`
 
-    # When the `core.nested` module is processed by CallGraphGenerator before the
-    # `_utils.parse` module, the graph that is generated refers to
-    # `parse_into_list_of_expressions` by two different fully qualified names:
-    # `_utils.parse.expr.parse_into_list_of_expressions` and
-    # `_utils.parse.parse_into_list_of_expressions`
     entrypoints = [
             "./fixtures/__init__.py",
             "./fixtures/_utils/parse/expr.py",
