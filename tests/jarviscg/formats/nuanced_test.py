0851f4b7d67e3dbb01bd19e0e13fe1ef28d525a7
Laila Winner <lailawinner@protonmail.ch>
2025-04-30 12:57:10 -0700

    Update test fixture to include correct file paths

--- tests/jarviscg/formats/nuanced_test.py
+++ tests/jarviscg/formats/nuanced_test.py
@@ -12,37 +12,37 @@ def test_nuanced_formatter_formats_graph() -> None:
     ]
     expected = {
         "fixtures.fixture_class": {
-            "filepath": os.path.abspath("fixtures/fixture_class.py"),
+            "filepath": os.path.abspath("tests/fixtures/fixture_class.py"),
             "callees": ["fixtures.fixture_class.FixtureClass"],
             "lineno": 1,
             "end_lineno": 11
         },
         "fixtures.other_fixture_class": {
-            "filepath": os.path.abspath("fixtures/other_fixture_class.py"),
+            "filepath": os.path.abspath("tests/fixtures/other_fixture_class.py"),
             "callees": ["fixtures.other_fixture_class.OtherFixtureClass", "fixtures.fixture_class"],
             "lineno": 1,
             "end_lineno": 6
         },
         "fixtures.other_fixture_class.OtherFixtureClass.baz": {
-            "filepath": os.path.abspath("fixtures/other_fixture_class.py"),
+            "filepath": os.path.abspath("tests/fixtures/other_fixture_class.py"),
             "callees": ["fixtures.fixture_class.FixtureClass.bar", "fixtures.fixture_class.FixtureClass.__init__"],
             "lineno": 4,
             "end_lineno": 6
         },
         "fixtures.fixture_class.FixtureClass.__init__": {
-            "filepath": os.path.abspath("fixtures/fixture_class.py"),
+            "filepath": os.path.abspath("tests/fixtures/fixture_class.py"),
             "callees": [],
             "lineno": 4,
             "end_lineno": 5
         },
         "fixtures.fixture_class.FixtureClass.bar": {
-            "filepath": os.path.abspath("fixtures/fixture_class.py"),
+            "filepath": os.path.abspath("tests/fixtures/fixture_class.py"),
             "callees": ["fixtures.fixture_class.FixtureClass.foo"],
             "lineno": 10,
             "end_lineno": 11
         },
         "fixtures.fixture_class.FixtureClass.foo": {
-            "filepath": os.path.abspath("fixtures/fixture_class.py"),
+            "filepath": os.path.abspath("tests/fixtures/fixture_class.py"),
             "callees": [],
             "lineno": 7,
             "end_lineno": 8
