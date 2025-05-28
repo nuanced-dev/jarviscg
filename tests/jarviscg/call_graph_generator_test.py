30340b179659eed0a724c5fcea8292e070e99a8c
Laila Winner <lailawinner@protonmail.ch>
2025-05-28 12:24:54 -0700

    Rename variable

--- tests/jarviscg/call_graph_generator_test.py
+++ tests/jarviscg/call_graph_generator_test.py
@@ -110,7 +110,7 @@ def test_call_graph_generator_dependency_analysis_disabled() -> None:
     ]
     dependency_called_function_name = "functools.cache"
     dependency_called_attribute_name = "datetime.datetime.now"
-    imported_called_name = "multiprocessing.Process"
+    imported_called_constructor_name = "multiprocessing.Process"
 
     cg = CallGraphGenerator(entrypoints, package)
     cg.analyze()
@@ -119,7 +119,7 @@ def test_call_graph_generator_dependency_analysis_disabled() -> None:
 
     callees = output["fixtures.fixture_class.FixtureClass.foo"]
     assert dependency_called_attribute_name not in callees
-    assert imported_called_name in callees
+    assert imported_called_constructor_name in callees
     assert dependency_called_function_name in callees
     assert output[dependency_called_function_name] == []
 
@@ -132,7 +132,7 @@ def test_call_graph_generator_decy_dependency_analysis_enabled() -> None:
     ]
     dependency_called_function_name = "functools.cache"
     dependency_called_attribute_name = "datetime.datetime.now"
-    imported_called_name = "multiprocessing.Process"
+    imported_called_constructor_name = "multiprocessing.Process"
 
     cg = CallGraphGenerator(entrypoints, package, decy=True)
     cg.analyze()
@@ -141,6 +141,6 @@ def test_call_graph_generator_decy_dependency_analysis_enabled() -> None:
 
     callees = output["fixtures.fixture_class.FixtureClass.foo"]
     assert dependency_called_attribute_name not in callees
-    assert imported_called_name not in callees
+    assert imported_called_constructor_name not in callees
     assert dependency_called_function_name in callees
     assert len(output[dependency_called_function_name]) > 0
